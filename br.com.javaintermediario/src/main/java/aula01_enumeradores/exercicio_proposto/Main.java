f7312ff40726eb4a84350fce75b0bd4dfc5c36af
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-08-03 23:39:28 -0300

    Criado a classe OperacaoMatematica

---
+++ br.com.javaintermediario/src/main/java/aula01_enumeradores/exercicio_proposto/Main.java
@@ -0,0 +1,16 @@
+package aula01_enumeradores.exercicio_proposto;
+
+public class Main {
+
+    // 1- Escreva um Enum que represente as quatro operações matemáticas: somar, subtrair, multiplicar e dividir.
+    // 2- O Enum deve ter um construtor que recebe o símbolo da operação (+, -, * e /)
+    // 3- Sobrescreva o método toString que retorne somente o símbolo da operação.
+    // 4- Implemente um método abstrato com a seguinte assinatura double executarOperacao(double x, double y).Cada
+    //opção do Enum deve implementar esse método realizando a operação de acordo com sua descrição.
+    // 5- Escreva um método main para teste que realize todas as operações do Enum. Dica: Você pode usar o método
+    //values para iterar o Enum e executar todas as operações.
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ br.com.javaintermediario/src/main/java/aula01_enumeradores/exercicio_proposto/OperacaoMatematica.java
@@ -0,0 +1,6 @@
+package aula01_enumeradores.exercicio_proposto;
+
+public enum OperacaoMatematica {
+
+    SOMAR, SUBTRAIR, MULTIPLICAR, DIVIDIR;
+}
