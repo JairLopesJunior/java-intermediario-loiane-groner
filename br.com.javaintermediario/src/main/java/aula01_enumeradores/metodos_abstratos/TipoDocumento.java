b17a746905c47c387e163a89f6be854e6e4bca16
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-08-03 23:26:54 -0300

    Trabalhando com metodos abstrato no Enum em Java

--- br.com.javaintermediario/src/main/java/aula01_enumeradores/metodos_abstratos/TipoDocumento.java
+++ br.com.javaintermediario/src/main/java/aula01_enumeradores/metodos_abstratos/TipoDocumento.java
@@ -2,5 +2,15 @@ package aula01_enumeradores.metodos_abstratos;
 
 public enum TipoDocumento {
 
-    CPF, CNPJ;
+    CPF{
+        public String geraNumeroTeste(){
+            return null;
+        }
+    }, CNPJ{
+        public String geraNumeroTeste() {
+            return null;
+        }
+    };
+
+    public abstract String geraNumeroTeste();
 }
