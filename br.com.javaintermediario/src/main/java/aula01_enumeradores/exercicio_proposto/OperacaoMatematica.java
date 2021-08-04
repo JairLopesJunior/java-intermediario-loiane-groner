8deb226713c7c17b58eff3b14eb631bd8e1fe803
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-08-03 23:52:54 -0300

    Sobresscrito o metodo substring e adicionado um novo metodo estatico em cada enum

--- br.com.javaintermediario/src/main/java/aula01_enumeradores/exercicio_proposto/OperacaoMatematica.java
+++ br.com.javaintermediario/src/main/java/aula01_enumeradores/exercicio_proposto/OperacaoMatematica.java
@@ -2,9 +2,27 @@ package aula01_enumeradores.exercicio_proposto;
 
 public enum OperacaoMatematica {
 
-    SOMAR("+"), SUBTRAIR("-"), MULTIPLICAR("*"), DIVIDIR("/");
+    SOMAR("+"){
+        double executarOperacao(double x, double y){
+            return x + y;
+        }
+    }, SUBTRAIR("-"){
+        double executarOperacao(double x, double y){
+            return x - y;
+        }
+    }, MULTIPLICAR("*"){
+        double executarOperacao(double x, double y){
+            return x * y;
+        }
+    }, DIVIDIR("/"){
+        double executarOperacao(double x, double y){
+            return x / y;
+        }
+    };
 
     private String simboloOperacao;
+
+    abstract double executarOperacao(double x, double y);
 
     OperacaoMatematica(String simboloOperacao){
         this.simboloOperacao = simboloOperacao;
@@ -12,5 +30,12 @@ public enum OperacaoMatematica {
 
     public String getSimboloOperacao() {
         return simboloOperacao;
+    }
+
+    @Override
+    public String toString() {
+        return "OperacaoMatematica{" +
+                "simboloOperacao='" + simboloOperacao + '\'' +
+                '}';
     }
 }
