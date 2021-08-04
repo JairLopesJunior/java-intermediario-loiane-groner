4eeaf82ab18693afd880fe92962cc20f85d767b2
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-08-03 23:43:28 -0300

    Criado o atributo simboloOperacao um construtor e o  metodo get

--- br.com.javaintermediario/src/main/java/aula01_enumeradores/exercicio_proposto/OperacaoMatematica.java
+++ br.com.javaintermediario/src/main/java/aula01_enumeradores/exercicio_proposto/OperacaoMatematica.java
@@ -2,5 +2,15 @@ package aula01_enumeradores.exercicio_proposto;
 
 public enum OperacaoMatematica {
 
-    SOMAR, SUBTRAIR, MULTIPLICAR, DIVIDIR;
+    SOMAR("+"), SUBTRAIR("-"), MULTIPLICAR("*"), DIVIDIR("/");
+
+    private String simboloOperacao;
+
+    OperacaoMatematica(String simboloOperacao){
+        this.simboloOperacao = simboloOperacao;
+    }
+
+    public String getSimboloOperacao() {
+        return simboloOperacao;
+    }
 }
